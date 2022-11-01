4750b20d765d90d82cf49a1472a6c9efeda37068
whyubiel <104115151+whyubiel@users.noreply.github.com>
2022-10-31 23:07:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+let familia = ['Gabriel','Nicolas','Gaab','Marcão',];
+let negado = false
+let salario = 1000;
+
+
+
+
+
+
+let pessoa = {
+    nome: 'Gabriel',
+    idade : 16,
+    estaAprovado: true ,
+    sobrenome : 'cortês'
+};
+
+let pessoa1 = {
+    nome: 'Nicolas',
+    idade : 18,
+    estaAprovado: negado ,
+    sobrenome : 'Gabriel'
+};
+
+let pessoa2 = {
+    nome: 'Gaab',
+    idade : 17,
+    estaAprovado: true ,
+    sobrenome : 'cortês'
+};
+
+let pessoa3 = {
+    nome: 'Marcão',
+    idade : 17,
+    estaAprovado: true ,
+    sobrenome : 'Caveira'
+};
+
+let familiastatus = "familia";
+function estaAprovado(negado){
+    let familia
+    familia = true;
+
+
+
+};
+console.log(salario * salario);
+
+
+
